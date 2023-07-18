b0e9d517dbff695cf241adff9dddc2102e7b8e96
Nikolay Georgiev <nikolay--georgiev@abv.bg>
2023-07-18 08:23:09 +0300

    Edit index page

--- DriverSchoolApp/Constants.cs
+++ DriverSchoolApp/Constants.cs
@@ -1,5 +1,20 @@
 ﻿namespace DriverSchoolApp;
 
-public class Constants
+public static class Constants
 {
+    public static class Contacts
+    {
+        public const string Name = "ПЕТЯ ВВС ЕООД";
+        public const string ShortLocation = "учебен център от Радомир,обл. Перник";
+        public const string Address = "Радомир, УЛ.ВИТОША № 1";
+        public const string PhoneNumber = "+359 888 397 025";
+        public const string Email = "add Email address";
+        public const string WorkingTime = "Пон - Пет : 09:00 ч. - 17:00 ч.";
+    }
+
+    public static class IndexPage
+    {
+
+    }
+
 }
