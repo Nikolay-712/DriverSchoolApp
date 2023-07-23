f23197a01a41e665733460caaaf8f6dd14467053
Nikolay Georgiev <nikolay--georgiev@abv.bg>
2023-07-23 09:17:35 +0300

    Edit contact page

--- DriverSchoolApp/Constants.cs
+++ DriverSchoolApp/Constants.cs
@@ -5,9 +5,11 @@ public static class Constants
     public static class Contacts
     {
         public const string Name = "ПЕТЯ ВВС ЕООД";
+        public const string OwnerName = "Станислав Корсачки";
         public const string ShortLocation = "учебен център от Радомир,обл. Перник";
         public const string Address = "Радомир, УЛ.ВИТОША № 1";
         public const string PhoneNumber = "+359 888 397 025";
+        public const string PhoneNumber2 = "+359 893 437 072";
         public const string Email = "add Email address";
         public const string WorkingTime = "Пон - Пет : 09:00 ч. - 17:00 ч.";
     }
@@ -15,6 +17,13 @@ public static class Constants
     public static class IndexPage
     {
 
+    }
+
+    public static class ContactsPage
+    {
+        public const string PageName = "Контакти";
+        public const string Title = "СВЪРЖЕТЕ СЕ С НАС";
+        public const string SubTitle = "Ако имате някакви въпроси, моля, свържете се с нас";
     }
 
 }
