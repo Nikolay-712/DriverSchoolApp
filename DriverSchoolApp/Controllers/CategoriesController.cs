52c5f43acdb984a74f0e604ab88c25ab9954e319
Nikolay Georgiev <nikolay--georgiev@abv.bg>
2024-05-19 12:32:37 +0300

    update app routs

--- DriverSchoolApp/Controllers/CategoriesController.cs
+++ DriverSchoolApp/Controllers/CategoriesController.cs
@@ -5,7 +5,7 @@ namespace DriverSchoolApp.Controllers;
 
 public class CategoriesController : Controller
 {
-    public IActionResult CategoryB()
+    public IActionResult Category_b()
     {
         Category_B category_B = new Category_B();
 
@@ -13,35 +13,35 @@ public class CategoriesController : Controller
     }
 
 
-    public IActionResult CategoryBAuto()
+    public IActionResult Category_b_auto()
     {
         Category_BAuto category_B = new Category_BAuto();
 
         return this.View(category_B);
     }
 
-    public IActionResult CategoryBE()
+    public IActionResult Category_be()
     {
         Category_BE category_BE = new Category_BE();
 
         return this.View(category_BE);
     }
 
-    public IActionResult CategoryC()
+    public IActionResult Category_c()
     {
         Category_C category_C = new Category_C();
 
         return this.View(category_C);
     }
 
-    public IActionResult CategoryCE()
+    public IActionResult Category_ce()
     {
         Category_CE category_CE = new Category_CE();
 
         return this.View(category_CE);
     }
 
-    public IActionResult CategoryRefresh()
+    public IActionResult Category_refresh()
     {
         return this.View();
     }
